aaa6b3ea33844c1d2d800a7e2212cb35ea32fe15
Jia Pan <jpan26@jh.edu>
2024-11-25 03:57:45 -0600

    make action interval min_RTT

--- src/salsify/fake-webcam.cc
+++ src/salsify/fake-webcam.cc
@@ -58,7 +58,7 @@ int main( int argc, char *argv[] )
     abort();
   }
 
-  if ( argc != 3 ) {
+  if ( argc != 2 ) {
     usage( argv[ 0 ] );
     return EXIT_FAILURE;
   }
@@ -69,14 +69,14 @@ int main( int argc, char *argv[] )
   IVFReader input {argv[1]};
 
   /* parse the # of frames per second of playback */
-  unsigned int frames_per_second = paranoid::stoul( argv[ 2 ] );
+  //unsigned int frames_per_second = paranoid::stoul( argv[ 2 ] );
 
   /* open the output */
   FileDescriptor stdout { STDOUT_FILENO };
 
-  const auto interval_between_frames = chrono::microseconds( int( 1.0e6 / frames_per_second ) );
+  //const auto interval_between_frames = chrono::microseconds( int( 1.0e6 / frames_per_second ) );
 
-  auto next_frame_is_due = chrono::system_clock::now();
+  //auto next_frame_is_due = chrono::system_clock::now();
 
   // Test encoder
   Encoder base_encoder { input.display_width(), input.display_height(),
@@ -99,13 +99,33 @@ int main( int argc, char *argv[] )
   uint32_t frame_no = 0;
   system_clock::time_point last_sent = system_clock::now();
 
+  /*TODO: Move into the congestion control class */
+  bool initialized = false;
+  uint32_t arrival = 0; // # of pkts
+  uint32_t service = 0; // # of pkts
+  //uint32_t loss = 0; // # of pkts
+  typedef double Time;		 
+  Time min_rtt = 0; // ms
+
+
+
+  auto start = chrono::system_clock::now();
+
+
   poller.add_action( Poller::Action( encode_pipe.second, Direction::In,
     [&]() -> Result {
       encode_pipe.second.read();
 
       /* wait until next frame is due */
-      this_thread::sleep_until( next_frame_is_due );
-      next_frame_is_due += interval_between_frames;
+      //this_thread::sleep_until( next_frame_is_due );
+      //next_frame_is_due += interval_between_frames;
+      std::chrono::duration<double, std::ratio<1, 1000>> diff = (system_clock::now() - last_sent);
+      if (diff.count() < min_rtt) {
+        encode_pipe.first.write( "1" );
+        return ResultType::Continue;
+      }
+      // TODO: minRtt passed, update belief bound!
+
 
 
       const Optional<RasterHandle> raster = input.get_next_frame();
@@ -118,7 +138,15 @@ int main( int argc, char *argv[] )
       //display.draw(rh);
     
       auto source_minihash = base_encoder.minihash();
-      vector<uint8_t> output = base_encoder.encode_with_quantizer( raster.get(), 100);
+      vector<uint8_t> output;
+      if (not initialized) {
+        // Probe with least amount of frames to get min RTT
+        output = base_encoder.encode_with_quantizer( raster.get(), 127);
+      } else {
+        output = base_encoder.encode_with_quantizer( raster.get(), 127);
+      }
+      // Add reed-solomon code here 
+
       auto target_minihash = base_encoder.minihash();
       //cout << rh.display_width() << " " << rh.display_height() << " " << output.size() << " " << endl;
 
@@ -127,15 +155,18 @@ int main( int argc, char *argv[] )
                            static_cast<uint32_t>( duration_cast<microseconds>( system_clock::now() - last_sent ).count() ),
                            output};
 
-      
+      // Sent out all the packets instanteneously 
       for ( const auto & packet : ff.packets() ) {
-        pacer.push( packet.to_string(), 2000u );
+        //pacer.push( packet.to_string(), 0 );
+        socket.send( packet.to_string() );
+        arrival += 1;
       } 
 
       last_sent = system_clock::now();
       ++frame_no;
-
-      encode_pipe.first.write( "1" );
+      if (initialized) {
+        encode_pipe.first.write( "1" );
+      }
 
       return ResultType::Continue;
     } ) 
@@ -146,18 +177,43 @@ int main( int argc, char *argv[] )
     {
       auto packet = socket.recv();
       AckPacket ack( packet.payload );
+      
+      if (ack.frame_no() == 80) {
+        std::chrono::duration<double> diff = (system_clock::now() - start);
+        cout << "Spent: " << diff.count() << endl;
+      }
+
 
       if ( ack.connection_id() != connection_id ) {
         /* this is not an ack for this session! */
         return ResultType::Continue;
       }
 
+      service += 1;
       cout << "Ack!" << ack.frame_no() <<" "<< ack.fragment_no() << endl;
+
+      // Need also to take loss into account
+      // Need to check content of the pkt to detect dup
+      if (arrival == service) {
+        if (not initialized) {
+          initialized = true;
+          std::chrono::duration<double, std::ratio<1,1000>> diff = (system_clock::now() - last_sent);
+          min_rtt = diff.count();
+          cout << "Initial min_rtt is [] " << min_rtt << endl;
+          encode_pipe.first.write( "1" );
+        } else {
+          std::chrono::duration<double, std::ratio<1,1000>> diff = (system_clock::now() - last_sent);
+          if (diff.count() < min_rtt) {
+            min_rtt = diff.count();
+            cout << "Updated min_rtt is [] " << min_rtt << endl;
+          }  
+        }
+      }
 
       return ResultType::Continue;
     } )
   );
-
+/*
   poller.add_action( Poller::Action( socket, Direction::Out, [&]() {
         assert( pacer.ms_until_due() == 0 );
 
@@ -165,12 +221,13 @@ int main( int argc, char *argv[] )
           assert( not pacer.empty() );
           //cout << "sent!" << endl;
           socket.send( pacer.front() );
+          arrival += 1;
           pacer.pop();
         }
 
         return ResultType::Continue;
     }, [&]() { return pacer.ms_until_due() == 0; } ) );
-
+*/
   
 
   // Start!!!
